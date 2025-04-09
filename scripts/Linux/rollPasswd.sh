aa989d10c22aef8e472f1f08e896c85b66566ead
Andrey Nikitin <143126464+pyukey@users.noreply.github.com>
2025-04-09 15:09:00 -0400

    Password change scripts

---
+++ scripts/Linux/rollPasswd.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+if id | grep -q "uid=0"; then
+  :
+else
+  echo "You must run this script as root!"
+  exit
+fi
+gen() {
+  echo "$(shuf -n 5 WORDLIST.TXT | paste -sd '0' -)"
+}
+out=""
+while getopts "f:" opt; do
+  case $opt in
+    f) users=$(grep -Ev 'blackteam' /etc/passwd | awk -F: '{print $1}');;
+    ?) users=$(grep -Ev '/sbin/nologin|/bin/false|sync|blackteam' /etc/passwd | awk -F: '{print $1}');;
+  esac
+done
+for user in $users
+do pass=$(gen)
+  if openssl passwd --help 2>&1 | grep -q '\6'; then
+    hashPass=$(openssl passwd -6 "$pass")
+    sed -i "s|^$user:[^:]*:|$user:$hashPass:|" /etc/shadow
+  else
+    (echo $pass; echo $pass) | passwd $user
+  fi
+  out="$out\n$user: $pass"
+done
+printf "$out\n"
